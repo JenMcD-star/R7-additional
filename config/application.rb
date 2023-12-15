609a1955aa4a3fdaa0ef1bb0aa7ce81a2f508938
Jennifer McDougall <jenniferannvancura@gmail.com>
2023-12-15 14:40:26 -0600

    fix font

--- config/application.rb
+++ config/application.rb
@@ -13,12 +13,5 @@ module R7Additional
 
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
